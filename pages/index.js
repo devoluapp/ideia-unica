aaaddbc9e39f53c8204d5e072fa17eccbabfd845
Roberto Cesar Tavares <roberto.tavares@trt2.jus.br>
2022-06-15 19:17:16 -0300

    primeira atualização

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import {useState} from 'react';
 function Home(){
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home Atualizado</h1>
             <Contador />
         </div>
     );
